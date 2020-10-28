a4d63a4b71c204c6b03820ab7e99daaf7b45b8f1
zhaoyang <yuanzhaoyang658@outlook.com>
2020-10-28 09:55:53 +0800

    first

--- wrapper2018.sh
+++ wrapper2018.sh
@@ -18,50 +18,33 @@ source cmssw_setup.sh
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
+cfg_file="cfg.py"
+wget --no-check-certificate "http://stash.osgconnect.net/+zhyuan/cfg_nanogen/SMP-RunIIFall18wmLHEGS-00138-cfg.py"  -O ${cfg_file}|| exit_on_error $? 150 "Could not download cfg file."
 
-wget --no-check-certificate --progress=bar "http://stash.osgconnect.net/+zhyuan/anaCondor/SMP-RunIIFall18wmLHEGS-00059_1_cfg.py"  || exit_on_error $? 150 "Could not download sandbox1."
-#cmsrel CMSSW_10_2_18
-#cd CMSSW_10_2_18/src
-#cmsenv
-#cd -
-# Download sandbox, replace it when you have different sandbox_name
-#sandbox_name1="sandbox-CMSSW_10_2_6-e33be22.tar.bz2"
-sandbox_name2="sandbox-CMSSW_10_2_18-441666a.tar.bz2"
-# sandbox_name3="sandbox-CMSSW_10_6_1-441666a.tar.bz2"
-# Change to your own http
-# wget --no-check-certificate --progress=bar "http://stash.osgconnect.net/+zguan/Wjets/condor/${sandbox_name1}"  || exit_on_error $? 150 "Could not download sandbox1."
-wget --no-check-certificate --progress=bar "http://stash.osgconnect.net/+zhyuan/anaCondor/${sandbox_name2}"  || exit_on_error $? 150 "Could not download sandbox1."
+# http://stash.osgconnect.net/+zhyuan/cfg_nanogen/sandbox-CMSSW_11_2_0_pre7-ead378f.tar.bz2
+sandbox_name="sandbox.tar.bz2"
+wget --no-check-certificate  "http://stash.osgconnect.net/+zhyuan/cfg_nanogen/sandbox-CMSSW_11_2_0_pre7-ead378f.tar.bz2" -O ${sandbox_name} || exit_on_error $? 150 "Could not download sandbox."
 
-pushd .
-# Setup framework from sandbox
-cmssw_setup $sandbox_name2 || exit_on_error $? 151 "Could not unpack sandbox"
-popd
-gridpack_name="DYJets_HT-$1_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz"
-wget --no-check-certificate --progress=bar "http://stash.osgconnect.net/+zhyuan/stitch_inclusive/mg265/${gridpack_name}"  || exit_on_error $? 150 "Could not download gridpack."
+Setup framework from sandbox
+cmssw_setup $sandbox_name || exit_on_error $? 151 "Could not unpack sandbox"
+scram b -j 10
 
-echo `pwd`
-sed -i "s/^.*tarball.tar.xz.*$/     args = cms.vstring(\'..\/$gridpack_name\'),/" -i SMP-RunIIFall18wmLHEGS-00059_1_cfg.py
+gridpack_name="dyellell${1}_5f_NLO_FXFX_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz"
+# gridpack_name="dyellell0j_5f_NLO_FXFX_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz"
+wget --no-check-certificate  "http://stash.osgconnect.net/+zhyuan/stitch_inclusive/dyjetNLO/mg265/${gridpack_name}"    || exit_on_error $? 150 "Could not download gridpack."
+
+
+sed -i "s/^.*tarball.tar.xz.*$/     args = cms.vstring(\'..\/$gridpack_name\'),/" -i ${cfg_file}
 
 seed=$(($(date +%s) +1))
-sed -i "s/process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=int(.*)$/process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=int($seed)/" -i SMP-RunIIFall18wmLHEGS-00059_1_cfg.py
+sed -i "s/process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=int(.*)$/process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=int($seed)/" -i ${cfg_file}
+sed -i "s/fileName = cms.untracked.string('file:SMP-RunIIFall18wmLHEGS-00138.root')/fileName = cms.untracked.string('file:265_${1}_${seed}.root')/" -i ${cfg_file}
+# cmsRun ${cfg_file}
 
-cmsRun SMP-RunIIFall18wmLHEGS-00059_1_cfg.py
-/bin/rm -rf $gridpack_name
-#cmsRun SMP-RunIIAutumn18DRPremix-00048_1_cfg.py
-#/bin/rm -rf SMP-RunIIFall18wmLHEGS-00059_inLHE.root SMP-RunIIFall18wmLHEGS-00059.root
-#cmsRun SMP-RunIIAutumn18DRPremix-00048_2_cfg.py
-#/bin/rm -rf SMP-RunIIAutumn18DRPremix-00048_step1.root
-#cmsRun SMP-RunIIAutumn18MiniAOD-00048_1_cfg.py
-#/bin/rm -rf SMP-RunIIAutumn18DRPremix-00048.root
-/bin/rm -rf cmssw-tmp
-# Setup framework from sandbox
-#pushd .
-#cmssw_setup $sandbox_name2 || exit_on_error $? 151 "Could not unpack sandbox"
-#popd
-#cmsRun SMP-RunIIAutumn18NanoAODv6-00019_1_cfg.py
-#/bin/rm -rf SMP-RunIIAutumn18MiniAOD-00048.root
-# clean
-/bin/rm -rf *py
-/bin/rm -rf *pyc
-#/bin/rm -rf $sandbox_name1
-/bin/rm -rf $sandbox_name2
+# /bin/rm -rf $gridpack_name
+# /bin/rm -rf cmssw-tmp
+# # clean
+# /bin/rm -rf *py
+# /bin/rm -rf *pyc
+# /bin/rm -rf $sandbox_name
+
