771fbeb7f1a47d32869958474046ef7526168a44
Zhaoyang Yuan <yuanzhaoyang658@outlook.com>
2020-11-25 15:05:51 +0800

    Update build_submit_gensim.py

--- build_submit_gensim.py
+++ build_submit_gensim.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
             outfile.write("Error = log/{0}.err_$(Cluster)-$(Process)\n".format(int_process))
             outfile.write("Output = log/{0}.out_$(Cluster)-$(Process)\n".format(int_process))
             outfile.write("Log = log/{0}.log_$(Cluster)\n".format(int_process))
-            outfile.write("transfer_output_remaps = \"{0} = {1}_$(Cluster)_$(Process).root\"\n".format(wrapper_version[_year],int_process))
+            outfile.write("transfer_output_remaps = \"{0} = root/{1}_$(Cluster)_$(Process).root\"\n".format(wrapper_version[_year],int_process))
             outfile.write("when_to_transfer_output = ON_EXIT\n")
             outfile.write("Queue 2\n")
         #os.system("curl http://stash.osgconnect.net/+jiexiao/wrapper_events.sh -o wrapper_{0}.sh".format(int_process))
